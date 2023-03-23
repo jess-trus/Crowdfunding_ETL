b96ab8e01326906741746efef2b5601bf97cc588
Pennsyd1 <118862894+Pennsyd1@users.noreply.github.com>
2023-03-23 19:02:18 -0400

    Add files via upload

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,42 +1,42 @@
-CREATE TABLE campaign (
-    cf_id INT  NOT NULL,
-    contact_id INT  NOT NULL,
-    company_name VARCHAR (100) NOT NULL,
-    description VARCHAR   NOT NULL,
-    goal DEC NOT NULL,
-    pledge DEC  NOT NULL,
-    outcome VARCHAR(50) NOT NULL,
-    backers_count INT  NOT NULL,
-    country VARCHAR(5)  NOT NULL,
-    currency VARCHAR(5)  NOT NULL,
-    launched_date DATE  NOT NULL,
-    end_date DATE  NOT NULL,
-    category_id VARCHAR(10)   NOT NULL,
-    subcategory_id VARCHAR(10)  NOT NULL,
-    PRIMARY KEY (cf_id),
-    FOREIGN KEY(contact_id) REFERENCES contacts(contact_id),
-    FOREIGN KEY(category_id) REFERENCES category(category_id),
-    FOREIGN KEY(subcategory_id) REFERENCES subcategory(subcategory_id)
+CREATE TABLE contacts (
+	contact_id integer PRIMARY KEY,
+ 	first_name varchar(30),
+	last_name varchar(30),
+	email varchar(50)
 );
-
-CREATE TABLE contacts(
-    contact_id INT NOT NULL,
-    first_name VARCHAR(30)  NOT NULL,
-    last_name VARCHAR(30)   NOT NULL,
-    email VARCHAR(100)  NOT NULL,
-    PRIMARY KEY(contact_id)
-   
-);
-
-CREATE TABLE category (
-    category_id VARCHAR(10) NOT NULL,
-    category VARCHAR(30) NOT NULL,
-    PRIMARY KEY (category_id)
-);
-
+--next table
 CREATE TABLE subcategory (
-    subcategory_id VARCHAR(15) NOT NULL,
-    subcategory VARCHAR(30) NOT NULL,
-    PRIMARY KEY (subcategory_id)
+	subcategory_id varchar(20) PRIMARY KEY,
+	subcategory varchar(20)
 );
 
+--next table
+CREATE TABLE category (
+	id varchar(20) PRIMARY KEY,
+	category varchar(20)	
+);
+
+--next table
+CREATE TABLE campaign (
+	cf_id integer,
+	contact_id int,
+	company_name varchar(50),
+	description text,	
+	goal real,
+	pledged real,
+	outcome varchar(20),
+	backers_count integer,
+	country varchar(10),
+	currency varchar(20),
+	launch_date date,
+	end_date date,
+	category_id varchar(10),
+	subcategory_id varchar(10),
+	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+	FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id),
+	FOREIGN KEY (category_id) REFERENCES category(id)
+);
+ 	SELECT* FROM campaign;
+	SELECT* FROM category;
+	SELECT* FROM subcategory;
+	SELECT* FROM contacts;
